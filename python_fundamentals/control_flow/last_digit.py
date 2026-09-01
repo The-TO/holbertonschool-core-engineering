ba33d0ee005a21147221183b281790f441a76bd1
The-TO <theovinc@gmail.com>
2026-09-01 15:26:26 +0200

    fix: adjust last digit calculation for negative numbers

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -3,6 +3,9 @@
 number = __import__('random').randint(-10000, 10000)
 digit = number % 10
 
+if number < 0 and digit != 0:
+    digit = digit - 10
+
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
 elif digit == 0:
