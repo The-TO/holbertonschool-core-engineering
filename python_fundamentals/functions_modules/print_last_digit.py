b51cd3fdedada3aeed512c3851d8a5fe180962a6
The-TO <theovinc@gmail.com>
2026-09-02 10:23:07 +0200

    feat: add print_last_digit function to print and return the last digit of a number

---
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    if number < 0:
+        digit = abs(number) % 10
+    else:
+        digit = number % 10
+    print("{}".format(digit))
+    return digit
