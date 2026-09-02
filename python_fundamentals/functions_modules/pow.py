38c4449277368e67b0ecda08d1271f4450f28243
The-TO <theovinc@gmail.com>
2026-09-02 12:42:04 +0200

    fix: correct return statement in pow function for negative exponent

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -5,9 +5,11 @@ def pow(a, b):
         result = 1
         for i in range(-b):
             result = result * a
+        return 1 / result
+    
     else:
         result = 1
         for i in range(b):
             result = result * a
-
     return result
+
