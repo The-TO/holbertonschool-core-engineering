63186d6b6eea54074b4988ed59a14856c7d2a5eb
The-TO <theovinc@gmail.com>
2026-09-07 16:28:29 +0200

    feat: add safe_print_division function to safely handle division and print results

---
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b 
+    except Exception:
+        print("Inside result: None")
+    finally:
+        print("Inside result: {}".format(result))
+        return result
