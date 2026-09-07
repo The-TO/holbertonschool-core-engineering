f22f0e9b4cbd3aeeee0d407eeee4bae43e81a352
The-TO <theovinc@gmail.com>
2026-09-07 16:30:01 +0200

    fix: handle division by zero by printing None in safe_print_division function

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     try:
         result = a / b 
     except Exception:
-        print("Inside result: None")
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
