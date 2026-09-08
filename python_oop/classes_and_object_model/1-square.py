e1ea2d5c7c1f5cb3c4ddc8b31edd869ee409b694
The-TO <theovinc@gmail.com>
2026-09-08 10:53:22 +0200

    feat: implement Square class with initialization method

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module 1-Square area"""
+
+
+class Square:
+    """Representation D'un Carre simple."""
+
+
+    def __init__(self, size):
+        """initialise la taille du Carre."""
+        self.__size = size
