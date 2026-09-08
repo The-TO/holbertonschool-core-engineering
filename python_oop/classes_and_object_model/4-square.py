816fb8d9fcc80f225c2d04e6f047f8f4fc6fc531
The-TO <theovinc@gmail.com>
2026-09-08 11:42:02 +0200

    fix: refactor size setter method for consistency in parameter naming

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -19,9 +19,9 @@ class Square:
         return self.__size * self.__size
 
     @size.setter
-    def size(self, value):
-        if type(value) is not int:
+    def size(self, size):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
