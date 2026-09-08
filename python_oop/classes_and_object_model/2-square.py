b7625eeb28c391b98b862dcda7653e6275ba642e
The-TO <theovinc@gmail.com>
2026-09-08 11:16:20 +0200

    feat: add area method to Square class for area calculation

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -12,3 +12,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Defini l'aire du Carre."""
+        return self.__size * self.__size
