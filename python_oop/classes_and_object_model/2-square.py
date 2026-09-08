ccf6c556d267fddd2a9449d83011ea9bbbe107fa
The-TO <theovinc@gmail.com>
2026-09-08 11:08:13 +0200

    fix: set default size parameter in Square class initialization method

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Representation d'un Carre simple."""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialise la taille du Carre."""
         if type(size) is not int:
             raise TypeError("size must be an integer")
