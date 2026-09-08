27ccd64a6cbb4f0697e78eebcdbadc51ce26c5f1
The-TO <theovinc@gmail.com>
2026-09-08 11:39:37 +0200

    feat: implement Square class with size validation and area calculation

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module 2-Square size verif."""
+
+
+class Square:
+    """Representation d'un Carre simple."""
+
+    def __init__(self, size=0):
+        """Initialise la taille du Carre."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter retourne la taille"""
+        return self.__size
+
+    def area(self):
+        """Defini l'aire du Carre."""
+        return self.__size * self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
