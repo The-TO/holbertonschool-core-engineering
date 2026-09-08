828b2bd62651acbad0f38472b968d0cc515d996b
The-TO <theovinc@gmail.com>
2026-09-08 17:13:22 +0200

    fix: update width and height setters to enforce positive values

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def width(self, value):
         if isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -33,28 +33,6 @@ class Rectangle:
     def height(self, value):
         if isinstance(value, int):
             raise TypeError ("height must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    def area(self):
-            """Defini l'aire du Rectangle."""
-            return self.__width * self.__height
-    
-    def perimeter(self):
-        return (self.__width + self.__height) * 2
-
-    def my_print(self):
-        print(self)
-
-    def __str__(self):
-        """Retourne la representation textuelle du Rectangle."""
-        if self.width == 0:
-            return ""
-        else:
-            lines =[]
-            for i in range(self.height):
-                lines.append("")
-            for i in range(self.width):
-                lines.append("#" * self.height + "#" * self.width)
-            return "\n".join(lines)
