e6d77d47cdd6a8bc23854e10a0fbb507f1e648e3
The-TO <theovinc@gmail.com>
2026-09-08 11:06:18 +0200

    fix: simplify conditional structure in Square class initialization method

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -9,6 +9,6 @@ class Square:
         """Initialise la taille du Carre."""
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0") 
         self.__size = size
