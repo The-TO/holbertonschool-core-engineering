292b35beb796e66750328f252adce27f3fead1d8
The-TO <theovinc@gmail.com>
2026-09-08 17:09:25 +0200

    fix: update width and height setters to allow zero values

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def width(self, value):
         if isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -33,8 +33,8 @@ class Rectangle:
     def height(self, value):
         if isinstance(value, int):
             raise TypeError ("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >=0")
+        if value <= 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
