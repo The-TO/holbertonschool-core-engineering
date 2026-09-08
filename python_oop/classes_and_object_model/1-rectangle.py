72c5bffa1f7f69f5f92ed64869fd04b0c1bb132c
The-TO <theovinc@gmail.com>
2026-09-08 17:03:51 +0200

    fix: add perimeter method to calculate rectangle perimeter

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -40,6 +40,9 @@ class Rectangle:
     def area(self):
             """Defini l'aire du Rectangle."""
             return self.__width * self.__height
+    
+    def perimeter(self):
+        return (self.width + self._height) * 2
 
     def my_print(self):
         print(self)
