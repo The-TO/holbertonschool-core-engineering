f5c7e92799573e2d40ff7377f881c0fafeb6e2de
The-TO <theovinc@gmail.com>
2026-09-09 15:56:11 +0200

    feat: add 2-square.py with Square class implementation and string representation

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Module 2-square
+Creation of Square class from the Rectangle module
+"""
+
+
+Rectangle = __import__('2-rectangle').Rectangle
+class Square(Rectangle):
+    """Initialise un Carré avec size"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        """Retourne une representation textuelle du Carré"""
+        return f.("[Square]{size}/{size}")
