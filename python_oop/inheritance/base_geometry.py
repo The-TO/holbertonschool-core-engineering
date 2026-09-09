9b9d40795810342b65cddc75187cb1203472c1d1
The-TO <theovinc@gmail.com>
2026-09-09 12:52:34 +0200

    fix: correct indentation in integer_validator method and update file permissions

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-            """Validation des parametres"""
+        """Validation des parametres"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value < 0:
