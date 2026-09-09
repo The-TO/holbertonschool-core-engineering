59a24dc3cffde1c3f21d298951524c9299aef27c
The-TO <theovinc@gmail.com>
2026-09-09 15:43:35 +0200

    fix: reorder method calls in Square class constructor for proper initialization

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -8,8 +8,8 @@ class Square(Rectangle):
     """Initialise un Carré avec size"""
     def __init__(self, size):
         self.__size = size
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
